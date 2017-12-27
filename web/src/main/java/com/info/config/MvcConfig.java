92ce92f15cb711918308094a2c27874b1a1a4d6e
lihao <lihay2007@sina.com>
2017-12-28 00:15:12 +0800

    add

--- web/src/main/java/com/info/config/MvcConfig.java
+++ web/src/main/java/com/info/config/MvcConfig.java
@@ -1,14 +1,24 @@
 package com.info.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 //@EnableWebMvc//无需使用该注解，否则会覆盖掉SpringBoot的默认配置值
-class WebMVCConfig extends WebMvcConfigurerAdapter {
+public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("/hello");
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(1000000);
+        return resolver;
     }
 }
